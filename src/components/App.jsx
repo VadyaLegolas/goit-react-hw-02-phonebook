976176913abb1e6945556eddc97ad27fdd875aef
Vlad <vadya-85@mail.ru>
2023-12-05 22:30:01 +0100

    add addContact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
@@ -6,19 +7,37 @@ export class App extends Component {
     name: '',
   };
 
-  handleChange = e => {};
+  handleChange = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+  handleAddContact = e => {
+    e.preventDefault();
+    const id = nanoid();
+    const contact = { id, name: this.state.name };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
 
   render() {
     return (
       <>
-      <form>
-        <input type="text" name="name" required onChange={this.handleChange} />
-        <button>Add contact</button>
-      </form>
-      <h2>Contacts</h2>
-      <ul>
-        <li></li>
-      </ul>
+        <form onSubmit={this.handleAddContact}>
+          <input
+            type="text"
+            name="name"
+            required
+            onChange={this.handleChange}
+          />
+          <br />
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => {
+            return <li key={contact.id}>{contact.name}</li>;
+          })}
+        </ul>
       </>
     );
   }

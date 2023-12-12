76d2689b60159119eb97d4d1e464db714674a351
Vlad <vadya-85@mail.ru>
2023-12-12 20:13:53 +0100

    Update ContactElement.styled.js

--- src/components/ContactElemet/ContactElement.styled.js
+++ src/components/ContactElemet/ContactElement.styled.js
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 
 export const ListItem = styled.li`
   width: 320px;
-
   margin-bottom: 10px;
-  padding-bottom: 5px;
   padding-left: 20px;
+
   &::marker {
     content: '»';
     color: blue;
@@ -17,12 +16,14 @@ export const Div = styled.div`
   display: flex;
   justify-content: space-between;
   border-bottom: 1px black dashed;
+  padding-bottom: 5px;
 `;
 
 export const Button = styled.button`
   border-radius: 10px;
   background-color: red;
   color: yellow;
+
   &:hover,
   &:focus {
     background-color: darkred;
